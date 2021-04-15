40a998c0c3806c1bef6f681cf552dd5de6a55334
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-04-15 14:25:58 -0300

    Including on Controller a call to EntregaService and developed reservaEntrega method on EntregaService

--- src/main/java/br/com/personal/microservice/transportador/controller/EntregaController.java
+++ src/main/java/br/com/personal/microservice/transportador/controller/EntregaController.java
@@ -1,16 +1,21 @@
 package br.com.personal.microservice.transportador.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.personal.microservice.transportador.controller.dto.EntregaDTO;
 import br.com.personal.microservice.transportador.controller.dto.VoucherDTO;
+import br.com.personal.microservice.transportador.service.EntregaService;
 
 @RestController
 @RequestMapping("/entrega")
 public class EntregaController {
 
+	@Autowired
+	private EntregaService entregaService;
+	
 	@RequestMapping(method = RequestMethod.POST)
 	public VoucherDTO reservaEntrega(EntregaDTO pedidoDTO){
 		return entregaService.reservaEntrega(pedidoDTO);
---
+++ src/main/java/br/com/personal/microservice/transportador/service/EntregaService.java
@@ -0,0 +1,23 @@
+package br.com.personal.microservice.transportador.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.personal.microservice.transportador.controller.dto.EntregaDTO;
+import br.com.personal.microservice.transportador.controller.dto.VoucherDTO;
+import br.com.personal.microservice.transportador.model.Entrega;
+import br.com.personal.microservice.transportador.repository.EntregaRepository;
+
+public class EntregaService {
+
+	@Autowired
+	private EntregaRepository entregaRepository;
+	
+	public VoucherDTO reservaEntrega(EntregaDTO pedidoDTO) {
+		
+		Entrega entrega = Entrega.receberPedido(pedidoDTO);
+		entregaRepository.save(entrega);
+		
+		return VoucherDTO.converterEntregar(entrega);
+	}
+
+}
