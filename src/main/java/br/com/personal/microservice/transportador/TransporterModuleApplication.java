41d850fce0017202936a5654ca962bcc94625521
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-04-15 14:04:50 -0300

    Creating a project of Transporting Module of Flower's Store

---
+++ src/main/java/br/com/personal/microservice/transportador/TransporterModuleApplication.java
@@ -0,0 +1,13 @@
+package br.com.personal.microservice.transportador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporterModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransporterModuleApplication.class, args);
+	}
+
+}
