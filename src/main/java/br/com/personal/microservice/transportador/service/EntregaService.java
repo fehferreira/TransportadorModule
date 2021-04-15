7a6ac3435d7b14794187cbc9858399bd710f2b37
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-04-15 15:36:42 -0300

    Adding a configuration of Config Server and Eureka and add annotation on EntregaService

--- src/main/java/br/com/personal/microservice/transportador/service/EntregaService.java
+++ src/main/java/br/com/personal/microservice/transportador/service/EntregaService.java
@@ -1,12 +1,14 @@
 package br.com.personal.microservice.transportador.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.personal.microservice.transportador.controller.dto.EntregaDTO;
 import br.com.personal.microservice.transportador.controller.dto.VoucherDTO;
 import br.com.personal.microservice.transportador.model.Entrega;
 import br.com.personal.microservice.transportador.repository.EntregaRepository;
 
+@Service
 public class EntregaService {
 
 	@Autowired
